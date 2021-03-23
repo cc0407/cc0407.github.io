362812541e28e99db255eb3264743f82b7cea907
Christian Catalano <christian@Christians-MacBook-Pro.local>
2021-03-23 15:48:44 -0400

    removed dummy code from body2, formatting change
    - Removed unnecessary code copied from Body1
    - Changed columns of body to so 1/3 width is on the right

--- src/views/HomeView/body2.tsx
+++ src/views/HomeView/body2.tsx
@@ -3,23 +3,10 @@ require("../../css/index.css");
 
 export const Body2 = () => {
     return (
-        <div className="full-page split page-light">
-      <div className="centered-column w-1/3">
-        <img className="circle-image"
-          src={'https://avatars.githubusercontent.com/u/46330616?s=400&u=d4130b152c2103dec9dadef214a11a50179c1c8f&v=4'} 
-        />
-        <div>That's me!</div>
+    <div className="full-page split page-light">
+      <div className="centered-column w-2/3">
       </div>
-      <div className="left-column w-2/3">
-        <div className="title">Hello!</div>
-        <div className="subtitle">
-          My name is Christian Catalano.
-        </div>
-        <div className="body2">
-          I am currently studying B.Comp Honours Software Engineering, 
-          minoring in Mathematics, at the University of Guelph. With 4+ years of relevant fulltime work experience,
-          I would be a great asset to your team!
-        </div>
+      <div className="left-column w-1/3">
       </div>
     </div>
     )
