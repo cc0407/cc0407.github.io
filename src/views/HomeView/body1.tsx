1e1d5373e714c77c48bd6fc7cac5682823e1fa2d
Christian Catalano <christian@Christians-MacBook-Pro.local>
2021-03-23 14:39:59 -0400

    Split home into two columns, added pic
    - Home is now split into two columns, 1/3 and 2/3.
    - Added circular image to left column, pulls directly from GitHub

--- src/views/HomeView/body1.tsx
+++ src/views/HomeView/body1.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React from "react"
 require("../../css/index.css");
 
 export const Body1 = () => {
   return (
-    <div className="full-page">
-      <div className="centered-column">
+    <div className="full-page split">
+      <div className="centered-column w-1/3">
+        <img className="circle-image"
+          src={'https://avatars.githubusercontent.com/u/46330616?s=400&u=d4130b152c2103dec9dadef214a11a50179c1c8f&v=4'} 
+        />
+        <div>That's me!</div>
+      </div>
+      <div className="left-column w-2/3">
         <div className="title">Hello!</div>
+        <div className="subtitle">
+          My name is Christian Catalano.
+        </div>
+        <div className="body2">
+          I am currently studying B.Comp Honours Software Engineering, 
+          minoring in Mathematics, at the University of Guelph. With 4+ years of relevant fulltime work experience,
+          I would be a great asset to your team!
+        </div>
       </div>
     </div>
   )
