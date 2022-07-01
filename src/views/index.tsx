48cd18dd94f057d93f9642f18d29276b2d74121b
cc0407 <cc0407@live.ca>
2022-07-01 17:49:55 -0400

    funky changes
    changed randomized letter to pull from array of languages.
    Translates 'Hello' to other languages

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -21,69 +21,76 @@ export const Homepage = () => {
   );
 };
 
-
+const language = [
+  {lang: "English", text: "Hello"},
+  {lang: "Italian", text: "Ciao"},
+  {lang: "Russian", text: "привет"},
+  {lang: "French", text: "Bonjour"},
+  {lang: "Finnish", text: "Hei"},
+]
 // This is the Body of the page, does not include header, nav, or footer
 // Custom props are defined in this interface
 interface props {}
 // Default values are defined in this interface
 const defaultProps: Partial<props> = {};
-
 export const Intro: React.FC<props> = () => {
-  
   /* Changes all letters except spaces in provided text to a random character */
   const nameRef = useRef();
-  const normalText = "Hey There"
-  function getFunky(e) {;
-    getFunkyHelper(normalText, normalText.length - 1);
+  function changeLanguage(e) {
+    const langSelect = Math.floor(Math.random() * (language.length));
+    setText(language[langSelect].text, language[langSelect].lang, nameRef);
   }
-  function getFunkyHelper(text: string, countDown: number) {
-    if(countDown < 0) return;
-    if(text[countDown] != ' ') {
-      var newChar = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-      var newText = text.substring(0, countDown) + newChar + text.substring(countDown + 1);
-
-      setText(newText);
-      getFunkyHelper(newText, countDown - 1);
-    } else {
-      getFunkyHelper(text, countDown - 1);
+  function setText(text: string, language: string | undefined, ref: any) {
+    if (ref.current) {
+      if(ref.current.innerText == text) {
+        changeLanguage(null);
+      }
+      else {
+        ref.current.innerText = text;
+        ref.current.title = language;
+      }
     }
   }
-  function getSerious(e) {
-    setText(normalText);
-  }
-  function setText(text) {
-    if(nameRef.current) nameRef.current.innerText = text;
-  };
   /*-----------------------------------------------------------------*/
 
   return (
-    <div className={"w-full column centered bg-offWhite min-h-128 flex-grow-default"}>
-
+    <div
+      className={
+        "w-full column centered bg-offWhite min-h-128 flex-grow-default"
+      }
+    >
       <div className="adjustableRow homeIntroRow homeIntroPadding">
         <div className="column centered py-4 800:px-10 flex-shrink-0">
           {/* DIV BELOW to be replaced with commented img tag, once i have a picture */}
-          <div className="circle-image"></div> 
+          <div className="circle-image"></div>
           {/* <img className="circle-image" src={''} /> */}
           <div className="font-light">^ That's me ^</div>
         </div>
 
         <div className=" column homeContent ">
           <div className="homeTitle min-w-[213px]">
-            <span ref={nameRef}  onMouseOver={getFunky} onMouseLeave={getSerious}>{normalText}</span>!
-            </div>
+            <span
+              ref={nameRef}
+              onMouseOver={changeLanguage}
+            >
+              Hello
+            </span>
+            !
+          </div>
           <div className="homeSubtitle">
             I'm <span className="font-bold">Christian Catalano</span>.
           </div>
           <div className="homeBody">
-            I am currently studying B.Comp Honours <span className="font-bold">Software Engineering</span>, minoring in <span className="font-bold">Mathematics</span>, at the University of Guelph. 
-            With <span className="font-bold">5+ years of full-time experience</span> in the industry,
-            and <span className="font-bold">3 years in full-stack development</span> specifically, I would be a great asset to your team!
+            I am currently studying B.Comp Honours{" "}
+            <span className="font-bold">Software Engineering</span>, minoring in{" "}
+            <span className="font-bold">Mathematics</span>, at the University of Guelph. With{" "}
+            <span className="font-bold">5+ years of full-time experience</span> in the industry, and{" "}
+            <span className="font-bold">3 years in full-stack development</span> specifically, I would be a great asset to your team!
           </div>
         </div>
       </div>
-
     </div>
-  )
+  );
 };
 
 Intro.defaultProps = defaultProps;
