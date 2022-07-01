37a91e53385722b4e8d0b06aa384277765305b64
cc0407 <cc0407@live.ca>
2022-07-01 11:27:10 -0400

    customized footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 interface props {
   className?: string;
@@ -16,33 +16,21 @@ export const Footer:React.FC<props> = (props) => {
     <div className={`footer ` + (props.homepage? `bg-black ` : `bg-darkBlue `) + `border-t-4 border-opacity-100 border-darkBlue2 column items-center justify-center ` + props.className}>
 
       <div className="social-buttons">
-        <a href="https://mobile.twitter.com/" target="blank" className="hover:text-LightGray2">
-          <FaTwitter className="cursor-pointer" size={'2em'} title="Twitter" />
+        <a href="https://github.com/cc0407" target="blank" className="hover:text-LightGray2">
+          <FaGithub className="cursor-pointer" size={'2em'} title="Twitter" />
         </a>
-        <a href="https://www.facebook.com/" target="blank" className="hover:text-LightGray2">
-          <FaFacebook className="cursor-pointer" size={'2em'} title="Facebook"/>
-        </a>
-        <a href="https://www.instagram.com/" target="blank" className="hover:text-LightGray2">
-          <FaInstagram className="cursor-pointer" size={'2em'} title="Instagram" />
+        <a href="https://www.linkedin.com/in/christian-catalano/" target="blank" className="hover:text-LightGray2">
+          <FaLinkedin className="cursor-pointer" size={'2em'} title="Facebook"/>
         </a>
       </div>
-    
-      <div className="footer-links text-LightGray3">
-        <a
-          className={"footer-link"}
-          target={"_self"}
-          href="/"
-        >
-          Link 1
-        </a>
-        <a
-          className={"footer-link"}
-          target={"_self"}
-          href="/"
-        >
-          Link 2
-        </a>
-      </div>
+
+      <a
+        className={"footer-link"}
+        target={"_self"}
+        href="/"
+      >
+        Contact
+      </a>
 
     </div>
   );
