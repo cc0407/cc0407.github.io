8031d8ff4ccfbb853be837bc7d29ff7ffeaf2034
cc0407 <cc0407@live.ca>
2022-10-16 17:12:07 -0400

    added component for work and personal pages
    Contains links to all of my public projects

--- src/components/AllProjects.tsx
+++ src/components/AllProjects.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 
-interface props {
-    type: "work" | "personal";
+interface projectTypes {
+    type: "work" | "personal"
 }
+interface props extends projectTypes {}
+
+interface project extends projectTypes {
+    name: string;
+    link: string;
+}
+const projectList:project[] = [
+    {name: "VIVA Streaming TV", link: "https://landing.vivatv.ca", type: 'work'},
+    {name: "TLN - Soccer Redesign", link: "https://tln.ca/soccer", type: 'work'},
+    {name: "Portfolio Website (this)", link: "/", type: 'personal'},
+    {name: "Russian Alphabet Practice", link: "/russian-alphabet-practice", type: 'personal'},
+    {name: "Tic-Tac-Toe Clone", link: "/tic-tac-toe", type: 'personal'},
+    {name: "Stars Arcade", link: "https://github.com/cc0407/stars-arcade", type: 'personal'},
+    {name: "Conway's Game of Life", link: "https://github.com/cc0407/stars-arcade", type: 'personal'},
+]
 
 // Default values are defined in this interface
 const defaultProps: Partial<props> = {};
@@ -11,6 +26,19 @@ export const AllProjects: React.FC<props> = (props) => {
     return (
         <div className="w-full column centered bg-offWhite min-h-128 flex-grow-default">
             <div className="adjustableRow homeIntroRow homeIntroPadding">
+                <ul>
+                    {projectList.map((project) => {
+                        if(project.type == props.type) {
+                            return(
+                                <li key={project.name}>
+                                    <a href={project.link} target="_blank">
+                                        {project.name}
+                                    </a>
+                                </li>
+                            )
+                        }
+                    })}
+                </ul>
             </div>
         </div>
     );
